a595527926a354caf47d5c02d0d58e975192f36e
Carlo Clamucha <carlo.clamucha@gmail.com>
2019-01-09 11:25:58 -0600

    added userModel folder

---
+++ backend/Routes/userRoutes.js
@@ -0,0 +1,5 @@
+const express = require('express');
+
+const server = express.Router();
+
+module.exports = router;
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 
+const userRoutes = require('./Routes/userRoutes');
+
 const server = express();
+
+const port = process.env.PORT || 4000;
+
+server.use('/user', userRoutes);
 
 server.listen(port, () => {
   console.log(`port running on ${port}`);
