8a70a16716d081a4abae7bf60cb49065ea1429ba
udykas <udykas@gmail.com>
2019-01-09 18:28:46 -0500

    Created Modal component

---
+++ frontend/src/components/Modal.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Modal extends Component {
+    constructor() {
+        super();
+    }
+
+    render () {
+        return (
+            <div>
+                This is a modal!
+            </div>
+        )
+    }
+}
+
+export default Modal;
