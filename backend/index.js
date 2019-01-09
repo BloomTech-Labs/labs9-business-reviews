32d55512156428adb417d5c33d2add0e091dcf55
bummings <eabummings@gmail.com>
2019-01-09 13:22:57 -0500

    added port variable, set up root route on index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
-
 const server = express();
+const knex = require('knex');
+const port = 9000;
+
+server.use(express.json());
+
+// R O O T  R O U T E
+server.get('/', (req, res) => {
+  res.send('API root.');
+});
 
 server.listen(port, () => {
   console.log(`port running on ${port}`);
