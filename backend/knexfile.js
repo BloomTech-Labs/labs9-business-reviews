b408bb2488fc8f4f88994f7f66b0a85e9b49f460
Carlo Clamucha <carlo.clamucha@gmail.com>
2019-01-09 13:44:08 -0600

    fixed file structure before pr

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -4,11 +4,11 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './db/dev.sqlite3'
+      filename: './dev.sqlite3'
     },
     useNullAsDefault: true,
     migrations: {
-      directory: './db/migrations'
+      directory: './migrations'
     }
   }
 
