c665c51e3394efc09354eb809f86d87bc2f3238f
bummings <eabummings@gmail.com>
2019-01-09 13:39:48 -0500

    added individual routes for business, user and reviews

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,11 @@ server.get('/', (req, res) => {
   res.send('API root.');
 });
 
+// R O U T E S
+server.use('/api/business', businessRouter);
+server.use('/api/user', userRouter);
+server.use('/api/review', reviewRouter);
+
 server.listen(port, () => {
   console.log(`port running on ${port}`);
 });
