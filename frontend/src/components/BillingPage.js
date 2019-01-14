1255cedbb7793537df78343ae255963b2b12979b
udykas <udykas@gmail.com>
2019-01-14 13:33:41 -0500

    Removed old form, that had been commented out

--- frontend/src/components/BillingPage.js
+++ frontend/src/components/BillingPage.js
@@ -44,22 +44,6 @@ class BillingPage extends Component {
         <StyledBilling>
           <SideBar />
           <div className="billing">
-            {/* <h1>Billing</h1>
-            <h4>Payment Info</h4>
-            <form className="payment-form">
-              <div className="input-div">
-                <p>CC#</p>
-                <input placeholder="####-####-####-####" />
-              </div>
-              <div className="input-div">
-                <p>EXP</p>
-                <input placeholder="MM/YY" />
-              </div>
-              <div className="input-div">
-                <p>CVV</p>
-                <input placeholder="###" />
-              </div>
-            </form> */}
             <form>
               <input type="radio" name="subscription" value="yearly" />1 Year Subscription - $9.99 <br/>
               <input type="radio" name="subscription" value="yearly" />1 Month Subscription - $0.99 <br/>
