c48a5fabd42535a2535c1eaf00739b8f7d458281
bummings <eabummings@gmail.com>
2019-01-15 13:57:20 -0500

    adds dependencies for postgres and configures production knex module

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,4 +1,6 @@
-// Update with your config settings.
+require('dotenv').config();
+
+const dbConnection = process.env.DATABASE_URL;
 
 module.exports = {
   development: {
@@ -13,37 +15,21 @@ module.exports = {
     seeds: {
       directory: './db/seeds'
     }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   }
-
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
-
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
 };
