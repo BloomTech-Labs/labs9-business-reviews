508fbaa5203cacec7f5261721e8bed54c7001471
udykas <udykas@gmail.com>
2019-01-14 19:40:25 -0500

    Set up back end for future use. Not currently functional. Also set up product, plan and a customer on Stripe Dashboard

---
+++ backend/stripe/customer.js
@@ -0,0 +1,6 @@
+// var stripe = require("stripe")(process.env.STRIPE_SECRET);
+
+// const consumer = stripe.customers.create({
+//     email: 'user_email',
+//     source: 'user_source',
+// });
---
+++ backend/stripe/product.js
@@ -0,0 +1,6 @@
+// var stripe = require("stripe")(process.env.STRIPE_SECRET);
+
+// const product = stripe.products.create({
+//     name: 'Bonafind Subscription',
+//     type: 'service',
+// });
