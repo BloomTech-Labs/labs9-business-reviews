97f07fa2f00734528c0d1aceb0654d4c08545b07
Brad Mortensen <b.mort86@gmail.com>
2019-01-14 17:44:09 -0700

    adds jest and first front end tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import App from './App';
 
-it('renders without crashing', () => {
+
+
+describe('<App />', () => {
+  it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+  });
+  it('should match snapshot', () => {
+    const tree = renderer.create(<App />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
 });
