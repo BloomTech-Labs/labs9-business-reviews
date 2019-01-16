4fba89d331250ba28c27496bd7ac043c30870dce
Brad Mortensen <b.mort86@gmail.com>
2019-01-16 12:52:56 -0700

    sorts data without mutating original

--- frontend/src/components/PopularBusinesses.js
+++ frontend/src/components/PopularBusinesses.js
@@ -16,7 +16,8 @@ export const PopularBusinessesStyles = styled.div`
 export const CardStyle = styled.div`
   margin-left: 15px;
   img {
-    max-width: 200;
+    max-width: 200px;
+    height: 200px;
   }
 `;
 
@@ -32,8 +33,9 @@ class PopularBusinesses extends Component {
     axios
       .get(`http://bonafind.herokuapp.com/api/business`)
       .then(response =>{
-        const filtered = response.data.filter(review => review.rating > 4.6); 
-        this.setState(() => ({ businesses: filtered })); 
+        const data = [...response.data]
+        const sortedAndSliced = data.sort((a, b) => a.rating < b.rating ? 1 : b.rating < a.rating ? -1 : 0).slice(0,4);
+        this.setState(() => ({ businesses: sortedAndSliced }));
       })
       .catch(err => {
         console.error('Server error: could not access users', err)
@@ -46,7 +48,7 @@ class PopularBusinesses extends Component {
         {this.state.businesses.map(({id, name, rating, image}) => (
           <CardStyle key={id}>
             <img src={image} alt="reviewed business"/>
-            <h1>{name}</h1>
+            <p>{name}</p>
             <p>{rating}</p>
           </CardStyle>
         ))}
