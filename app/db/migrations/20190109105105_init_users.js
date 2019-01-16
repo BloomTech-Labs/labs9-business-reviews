7f6b6431df28a211a8a2ebe844ec091f50f28569
Brad Mortensen <b.mort86@gmail.com>
2019-01-15 17:18:14 -0700

    updated migrations again

--- app/db/migrations/20190109105105_init_users.js
+++ app/db/migrations/20190109105105_init_users.js
@@ -1,6 +1,6 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', user => {
-    user.increments();
+    user.increments('id');
     user.string('name').notNullable();
     user.string('email').unique();
     user.string('password').notNullable();
