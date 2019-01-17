115a3ae2fed647ce4b259e6ffa7ff377605f8a98
Brad Mortensen <b.mort86@gmail.com>
2019-01-17 11:57:30 -0700

    extends header across screen

--- frontend/src/components/LandingNav.js
+++ frontend/src/components/LandingNav.js
@@ -5,41 +5,42 @@ import logo from '../assets/logo.png';
 const NavBar = styled.div`
   width: 100%;
   @import url('https://fonts.googleapis.com/css?family=Quicksand:300,500');
-  background-color: #eed974;
-  padding: 1rem 2rem;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  flex-direction: row;
-  font-family: 'Quicksand';
-  margin-bottom: 2rem;
-
-  .menu__logo--logo {
+  background-color: #eed974;  
+  .inner-nav-container{
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 1rem 2rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: row;
+    font-family: 'Quicksand';
+    margin-bottom: 2rem;
+    .menu__logo--logo {
     height: 150px;
     width: 150px;
     border-radius: 50%;
-  }
-
-  .menu__menuItems {
-    height: 20px;
-    font-size: 1.4rem;
-    display: flex;
-    align-items: center;
-    flex-direction: row;
-    text-transform: uppercase;
-    a {
-      color: black;
-      text-decoration: none;
-      margin: 10px;
     }
-    a:hover {
-      letter-spacing: 2px;
-      border-top: 1px solid black;
-      border-bottom: 1px solid black;
-      transition: all 280ms ease-in-out;
-    }
+    .menu__menuItems {
+      height: 20px;
+      font-size: 1.4rem;
+      display: flex;
+      align-items: center;
+      flex-direction: row;
+      text-transform: uppercase;
+      a {
+        color: black;
+        text-decoration: none;
+        margin: 10px;
+      }
+      a:hover {
+        letter-spacing: 2px;
+        border-top: 1px solid black;
+        border-bottom: 1px solid black;
+        transition: all 280ms ease-in-out;
+      }
+    }  
   }
-
   .menu__user--text {
     margin-right: 1rem;
     color: black;
