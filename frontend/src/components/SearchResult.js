cc31c0a79939fc5c6de5fec8865eeab5dcf03e2f
bummings <eabummings@gmail.com>
2019-01-22 14:09:34 -0500

    changes SearchResult component to functional component

--- frontend/src/components/SearchResult.js
+++ frontend/src/components/SearchResult.js
@@ -1,22 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class SearchResult extends Component {
-  constructor({ business }) {
-    super();
-    this.state = {
-      business: business
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>{this.props.business.id}</h1>
-        <img src={this.props.business.photos[0]} alt='bruh' />
-        <h2>{this.props.business.rating}</h2>
-        <h3>{this.props.business.adr_address}</h3>
-      </div>
-    );
-  }
-}
+const SearchResult = props => {
+  return (
+    <div>
+      <h1>{props.business.id}</h1>
+      <h2>{props.business.rating}</h2>
+      {/* {props.business.photos.map(photo => (
+        <h1>map</h1>
+      ))} */}
+    </div>
+  );
+};
 export default SearchResult;
