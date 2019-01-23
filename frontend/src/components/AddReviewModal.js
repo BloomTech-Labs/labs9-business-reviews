0a2de29fef10331c5d2e49931ae2246bc3c16bc6
Brad Mortensen <b.mort86@gmail.com>
2019-01-23 09:53:39 -0700

    adds basic review modal

--- frontend/src/components/AddReviewModal.js
+++ frontend/src/components/AddReviewModal.js
@@ -1,18 +1,42 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
-function AddReviewModal() {
-  return (
-    <div>
-      <form>
-        <label htmlFor="review-title">Title</label>
-        <input className="review-title"/>
-        <label htmlFor="review-body"></label>
-        <input className="review-body"/>
-        <button>Submit Review</button>
-        <button>Cancel</button>
-      </form>
-    </div>
-  )
+const ModalStyles = styled.div`
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	background: rgba(4, 0, 0, 0.7);
+	.review-modal {
+		width: 60%;
+		height: 200px;
+		border: 1px solid grey;
+		display: flex;
+		flex-flow: row wrap;
+		justify-content: space-around;
+		background-color: white;
+	}
+`;
+function AddReviewModal({toggleReviewing}) {
+	return (
+		<ModalStyles>
+			<div className="review-modal">
+        <h1>Add A Review</h1>
+				<form>
+					<label htmlFor="review-title">Title</label>
+					<input className="review-title" />
+					<label htmlFor="review-body" />
+					<input className="review-body" />
+					<button>Submit Review</button>
+					<button onClick={toggleReviewing}>Cancel</button>
+				</form>
+			</div>
+		</ModalStyles>
+	);
 }
 
 export default AddReviewModal;
