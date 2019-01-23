bdac9f98a788b8d7ca68c9291bcf59fa33d606e7
udykas <udykas@gmail.com>
2019-01-23 13:22:58 -0500

    Added post request to stripeRouter and a post request in the billing form component on the front end

--- app/stripe/stripeRouter.js
+++ app/stripe/stripeRouter.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = exprress.Router();
+const stripe = require('./stripe')
 
 const db = require('../db/dbinit');
 
-router.post('/', (req, res) => {
-
+router.post('/yearly', async(req, res) => {
+const {token} = req.query;
+const res = await stripe.create.charges({
+    amount:'999',
+    source: token,
+    currency:'USD'
+})
+res.json({res});
 });
 
 
--- frontend/src/components/BillingForm.js
+++ frontend/src/components/BillingForm.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 import styled from 'styled-components'
-// import { Mutation } from 'react-apollo';
-// import Router from 'next/router';
-// import NProgress from 'NProgress';
-// import gql from 'graphql-tag';
+import axios from 'axios';
 
 const StyledBillingForm = styled.div`
   .billing-form{
@@ -36,7 +33,11 @@ class BillingForm extends React.Component {
     console.log('On Token Called!');
     console.log(res);
     console.log(res.id);
-  }
+    axios
+      .post(`http:localhost:9000/api/billing/yearly?token=${res.id}`)
+      .then(res => consoloe.log(res))
+      .catech(err => console.log(err))
+  };
 
   render() {
     return(
