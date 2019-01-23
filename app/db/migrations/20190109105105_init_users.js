fe3c1c82e9d77debb19d382d033f63e2ab9f2248
Carlo Clamucha <carlo.clamucha@gmail.com>
2019-01-23 17:58:00 -0600

    pulled again

--- app/db/migrations/20190109105105_init_users.js
+++ app/db/migrations/20190109105105_init_users.js
@@ -4,14 +4,7 @@ exports.up = function(knex, Promise) {
     user.string('name').notNullable();
     user.string('email').unique();
     user.string('password').notNullable();
-<<<<<<< HEAD
-    user
-      .string('billing_id')
-      .defaultTo(null)
-      .unique();
-=======
     user.string('billing_id').defaultTo(null);
->>>>>>> 96e5e78fa1e018903e815dfa228729c3171e4559
     user.string('reset_token');
     user.string('gravatar');
   });
