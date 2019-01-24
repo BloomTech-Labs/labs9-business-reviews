bf0931877967cb14e1c2330081e2f97b93251a07
udykas <udykas@gmail.com>
2019-01-24 13:36:26 -0500

    Edited submit function

--- frontend/src/components/CheckoutForm.js
+++ frontend/src/components/CheckoutForm.js
@@ -4,11 +4,19 @@ import {CardElement, injectStripe} from 'react-stripe-elements';
 class CheckoutForm extends Component {
   constructor(props) {
     super(props);
+    this.state = {complete: false};
     this.submit = this.submit.bind(this);
   }
 
   async submit(ev) {
-    // User clicked submit
+    let {token} = await this.props.stripe.createToken({name: "Name"});
+    let response = await fetch("/charge", {
+        method: "POST",
+        headers: {"Content-Type": "text/plain"},
+        body: token.id
+    });
+
+    if (response.ok) console.log("Purchase Complete!")
   }
 
   render() {
