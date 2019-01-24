348de72a7d418610d51cc6c9a63cf05df9e61f01
udykas <udykas@gmail.com>
2019-01-24 13:49:11 -0500

    Added state to component and added setState to the submit function

--- frontend/src/components/CheckoutForm.js
+++ frontend/src/components/CheckoutForm.js
@@ -16,7 +16,7 @@ class CheckoutForm extends Component {
         body: token.id
     });
 
-    if (response.ok) console.log("Purchase Complete!")
+    if (response.ok) this.setState({complete: true});
   }
 
   render() {
