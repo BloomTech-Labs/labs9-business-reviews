5f986fc93bd2b16993ef74b4b890e95b5b39ae63
Brad Mortensen <b.mort86@gmail.com>
2019-01-24 15:15:42 -0700

    adds axios call to edit review form

--- frontend/src/components/EditReview.js
+++ frontend/src/components/EditReview.js
@@ -1,15 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Axios from 'axios';
+import {backendLink} from '../assets/config'
 
 export default class EditReview extends Component {
+  constructor(){
+    super();
+    this.state={
+      reviews: []
+    } 
+  }
   componentDidMount(){
     const {id} = this.props.match.params;
-    console.log(id)
+    Axios.get(`${backendLink}/api/review/${id}`)
+			.then((res) => this.setState({ reviews: res.data }))
+			.catch((err) => console.log(err));
   }
   render() {
     return (
       <div className="edit-container">
         <form>
-          <input></input>
+          <input />
+          <textarea />
         </form>
       </div>
     )
