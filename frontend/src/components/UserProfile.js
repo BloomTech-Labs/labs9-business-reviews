ca6be199fa626ce75289a329a768fcfa28979c02
Brad Mortensen <b.mort86@gmail.com>
2019-01-24 09:47:45 -0700

    removes bloat from user profile component

--- frontend/src/components/UserProfile.js
+++ frontend/src/components/UserProfile.js
@@ -1,44 +1,31 @@
-import React from 'react'
-import ProfileNav from './ProfileNav'
-import SideBar from './SideBar'
-import MyReviews from './MyReviews'
-import styled from 'styled-components'
+import React from 'react';
+import ProfileNav from './ProfileNav';
+import SideBar from './SideBar';
+import MyReviews from './MyReviews';
+import styled from 'styled-components';
 
 const StyledContainer = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  height: 600px;
-  margin-top: 20px;
-  .openModal{
-    width: 200px;
-    height: 100px;
-  }
+	width: 100%;
+	display: flex;
+	justify-content: space-between;
+	height: 600px;
+	margin-top: 20px;
+	.openModal {
+		width: 200px;
+		height: 100px;
+	}
 `;
 
-class UserProfile extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { isOpen: false };
-  }
-
-  toggleModal = () => {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
-  render() {
-    return (
-      <div>
-        <ProfileNav />
-        <StyledContainer>
-          <SideBar />
-          <MyReviews />
-          
-        </StyledContainer>
-      </div>
-    )
-  }  
+function UserProfile() {
+	return (
+		<div>
+			<ProfileNav />
+			<StyledContainer>
+				<SideBar />
+				<MyReviews />
+			</StyledContainer>
+		</div>
+	);
 }
 
 export default UserProfile;
