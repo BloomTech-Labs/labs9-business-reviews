70c21a68af5fd17258c9b7ca452bad64b8fa7820
Brad Mortensen <b.mort86@gmail.com>
2019-01-24 11:26:22 -0700

    adding review styles

--- frontend/src/components/MyReviews.js
+++ frontend/src/components/MyReviews.js
@@ -1,62 +1,47 @@
-import React, { Component } from 'react'
-import styled from 'styled-components'
-import Axios from 'axios'
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import Axios from 'axios';
 import { backendLink } from '../assets/config';
-import PlaceHolderReviews from './PlaceHolderReviews'
+import PlaceHolderReviews from './PlaceHolderReviews';
 
 const StyledReviews = styled.div`
-  width: 80%;
-  display: flex;
-  justify-content: center;
-  .add-review {
-    display: flex; 
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    h1{
-
-    }
-    button {
-      height: 40px;
-      width: 40px;
-      background-color: white;
-      border-radius: 50%;
-      font-size: 30px;
-      font-weight: bold;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-    }
-  }
+	width: 80%;
+	display: flex;
+	justify-content: center;
+	
 `;
 
 class MyReviews extends Component {
-  constructor(){
-    super();
-    this.state={
-      reviews:[]
-    }
-  }
-  componentDidMount() {
-    const id = this.props.id;
-    Axios.get(
-      `${backendLink}/api/user/${id}/reviews`
-    )
-      .then(res => this.setState({ reviews: res.data }))
-      .catch(err => console.log(err));  
-  }
-  render() {
-    return (
-      <StyledReviews>
-        {this.state.reviews?this.state.reviews.map(({title, body, image, id, rating}) => (                <div key={id}className='review'>                      
-          <div className='review-img1' />
-          <h4>{title}</h4>
-          <p>{body}</p>
-          <p>{`${rating} stars`}</p>            
-        </div>)):<PlaceHolderReviews/>}
-      </StyledReviews>
-    )
-  }
+	constructor() {
+		super();
+		this.state = {
+			reviews: []
+		};
+	}
+	componentDidMount() {
+		const id = this.props.id;
+		Axios.get(`${backendLink}/api/user/${id}/reviews`)
+			.then((res) => this.setState({ reviews: res.data }))
+			.catch((err) => console.log(err));
+	}
+	render() {
+		return (
+			<StyledReviews>
+				{this.state.reviews ? (
+					this.state.reviews.map(({ title, body, image, id, rating }) => (
+						<div key={id} className="review">
+							<div className="review-img1" />
+							<h4>{title}</h4>
+							<p>{body}</p>
+							<p>{`${rating} stars`}</p>
+						</div>
+					))
+				) : (
+					<PlaceHolderReviews />
+				)}
+			</StyledReviews>
+		);
+	}
 }
 
-export default MyReviews; 
+export default MyReviews;
