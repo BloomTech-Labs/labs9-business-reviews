15770c752e9144b19a8769ba939a32ae3a37e800
bummings <eabummings@gmail.com>
2019-01-24 17:48:30 -0500

    works out styles for user reviews

--- app/db/migrations/20190115171315_reviews.js
+++ app/db/migrations/20190115171315_reviews.js
@@ -1,13 +1,19 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('reviews', review => {
-      review.increments('id');
-      review.string('title').notNullable();
-      review.string('body', 1000).notNullable();
-      review.float('rating');
-      review.string('business_image');
-      review.string('business_name');
-      review.integer('business_id').references('id').inTable('businesses');
-      review.integer('reviewer_id').references('id').inTable('users');
+    review.increments('id');
+    review.string('title').notNullable();
+    review.string('body', 1000).notNullable();
+    review.float('rating');
+    review.string('business_image');
+    review.string('business_name');
+    review
+      .integer('business_id')
+      .references('id')
+      .inTable('businesses');
+    review
+      .integer('reviewer_id')
+      .references('id')
+      .inTable('users');
   });
 };
 exports.down = function(knex, Promise) {
--- app/db/seeds/003-reviews.js
+++ app/db/seeds/003-reviews.js
@@ -1,94 +1,92 @@
-exports.seed = function (knex, Promise) {
+exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('reviews')
     .truncate()
-    .then(function () {
+    .then(function() {
       // Inserts seed entries
       return knex('reviews').insert([
         {
-          title: "fish?!",
-          body: "this aint no fish",
+          title: 'fish?!',
+          body: 'this aint no fish',
           rating: 1,
-          business_id: "ChIJWXerz2vIxokRkue1--XSK5M",
-          reviewer_id: 4
-        },
-        {
-          title: "i like the fish",
-          body: "i like to eat the fish here",
-          rating: 4,
-          business_id: "ChIJWXerz2vIxokRkue1--XSK5M",
+          business_image:
+            'https://www.dibrunocatering.com/wp-content/uploads/2014/07/theoriginalphoto-2048x1418.jpg',
+          business_name: 'La Columbe Fishtown',
+          business_id: 'ChIJWXerz2vIxokRkue1--XSK5M',
           reviewer_id: 1
         },
         {
-          title: "fishtown",
-          body: "too many fish in this town",
+          title: 'fishtown',
+          body: 'too many fish in this town',
           rating: 1,
-          business_id: "ChIJWXerz2vIxokRkue1--XSK5M",
+          business_id: 'ChIJWXerz2vIxokRkue1--XSK5M',
           reviewer_id: 2
         },
         {
-          title: "pretty good",
-          body: "the most cheese ive ever had",
+          title: 'pretty good',
+          body: 'the most cheese ive ever had',
           rating: 4.7,
-          business_id: "ChIJfXqUmjHGxokReInkqMednds",
-          reviewer_id: 3
+          business_image:
+            'http://localhost:3000/business/ChIJfXqUmjHGxokReInkqMednds',
+          business_name: 'DiBruno Brothers',
+          business_id: 'ChIJfXqUmjHGxokReInkqMednds',
+          reviewer_id: 1
         },
         {
-          title: "too many cheese",
-          body: "ive never seen to many cheese in my life",
+          title: 'too many cheese',
+          body: 'ive never seen to many cheese in my life',
           rating: 2,
-          business_id: "ChIJfXqUmjHGxokReInkqMednds",
+          business_id: 'ChIJfXqUmjHGxokReInkqMednds',
           reviewer_id: 2
         },
         {
-          title: "Cheesey",
-          body: "i love cheese and this place",
+          title: 'my fav spot',
+          body: 'gets all me records here',
           rating: 5,
-          business_id: "ChIJfXqUmjHGxokReInkqMednds",
+          business_image:
+            'http://thekey.xpn.org/aatk/files/2017/05/IMG_9639-620x413.jpg',
+          business_name: 'Brewery Town Beats',
+          business_id: 'ChIJe2CFfLnHxokR3elHkyfA868',
           reviewer_id: 1
         },
         {
-          title: "my fav spot",
-          body: "gets all me records here",
-          rating: 5,
-          business_id: "ChIJe2CFfLnHxokR3elHkyfA868",
-          reviewer_id: 1
-        },
-        {
-          title: "1 star",
-          body: "too far from my house",
+          title: '1 star',
+          body: 'too far from my house',
           rating: 1,
-          business_id: "ChIJe2CFfLnHxokR3elHkyfA868",
+          business_id: 'ChIJe2CFfLnHxokR3elHkyfA868',
           reviewer_id: 2
         },
         {
-          title: "no selection",
-          body: "I couldnt find the record I wanted",
+          title: 'no selection',
+          body: 'I couldnt find the record I wanted',
           rating: 1,
-          business_id: "ChIJe2CFfLnHxokR3elHkyfA868",
+          business_id: 'ChIJe2CFfLnHxokR3elHkyfA868',
           reviewer_id: 4
         },
         {
-          title: "Its Wallmart",
-          body: "very bad experience here, too many peeple",
+          title: 'The El Bar',
+          body: 'very bad experience here, too many peeple',
           rating: 1,
-          business_id: "ChIJS38mY9-Ga4cR5uumnlSu3kw",
+          business_image:
+            'http://fishtownbeerrunners.com/wp-content/uploads/2015/12/The-El-Bar-.jpg',
+          business_name: 'The El Bar',
+          business_id: 'ChIJS38mY9-Ga4cR5uumnlSu3kw',
           reviewer_id: 1
         },
         {
-          title: "Pretty grate",
-          body: "Walmart if my favrite store",
+          title: 'Pretty grate',
+          body: 'Walmart if my favrite store',
           rating: 5,
-          business_id: "ChIJS38mY9-Ga4cR5uumnlSu3kw",
+          business_id: 'ChIJS38mY9-Ga4cR5uumnlSu3kw',
           reviewer_id: 3
         },
         {
-          title: "meh",
-          body: "its okay",
+          title: 'meh',
+          body: 'its okay',
           rating: 3,
-          business_id: "ChIJS38mY9-Ga4cR5uumnlSu3kw",
+          business_id: 'ChIJS38mY9-Ga4cR5uumnlSu3kw',
           reviewer_id: 4
-        },
+        }
       ]);
     });
 };
