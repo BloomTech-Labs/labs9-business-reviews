cb3576322562771f38b48ddd4a00e9f59e11f40f
bummings <eabummings@gmail.com>
2019-01-24 23:50:27 -0500

    adjusts footer to stick out less

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,23 +1,28 @@
-import React from 'react'
-import styled from 'styled-components'
-
+import React from 'react';
+import styled from 'styled-components';
 
 const FooterStyles = styled.div`
   width: 100%;
   display: flex;
-  justify-content: center;
+  justify-content: flex-end;
   a {
     margin-top: 150px;
     margin-bottom: 50px;
+    color: black;
+    text-decoration: none;
+    margin-right: 10px;
+    padding: 5px;
   }
 `;
 
 function Footer() {
   return (
     <FooterStyles>
-      <a href="https://github.com/Lambda-School-Labs/labs9-business-reviews">Meet the Team</a>
+      <a href='https://github.com/Lambda-School-Labs/labs9-business-reviews'>
+        Meet the Team
+      </a>
     </FooterStyles>
-  )
+  );
 }
 
 export default Footer;
--- frontend/src/components/MainContent.js
+++ frontend/src/components/MainContent.js
@@ -1,16 +1,15 @@
-import React from 'react'
-import PopularReviewers from './PopularReviewers'
-import PopularBusinesses from './PopularBusinesses'
+import React from 'react';
+import PopularReviewers from './PopularReviewers';
+import PopularBusinesses from './PopularBusinesses';
 // import FeaturedReviews from './FeaturedReviews'
 
 function MainContent() {
   return (
     <div>
-      {/* <FeaturedReviews /> */}
       <PopularBusinesses />
       <PopularReviewers />
     </div>
-  )
+  );
 }
 
 export default MainContent;
