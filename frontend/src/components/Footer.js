72090f9dd7737fa29b71274b4de3425e17356820
udykas <udykas@gmail.com>
2019-01-29 18:23:18 -0500

    Copyright symbol added to footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
+import copyright from '../assets/copyright.png';
 
 const FooterStyles = styled.div`
   width: 100%;
   display: inline-flex;
   justify-content: center;
+  align-items: center;
   padding-top: 50px;
   padding-bottom: 50px;
   background: #142e41;
   span{
     color: white;
-    margin-top: 5px;
     padding-right: 40px;
   }
   a {
     color: white;
     text-decoration: none;
     padding: 5px;
+  }
+  .copyright {
+    padding-right: 5px;
+    height: 15px;
+    width: 15px;
   }
 `;
 
@@ -25,7 +31,8 @@ const FooterStyles = styled.div`
 function Footer() {
   return (
     <FooterStyles>
-      <span>&copy Bonafind</span> 
+      <img src={copyright} alt="copyright" className="copyright" />
+      <span> Bonafind</span> 
       <a href='https://github.com/Lambda-School-Labs/labs9-business-reviews'>
         Meet the Team
       </a>
