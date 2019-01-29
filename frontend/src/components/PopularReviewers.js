2343f3190cb60840b0f127fc4e056e1a13a1a956
udykas <udykas@gmail.com>
2019-01-29 12:26:59 -0500

    Added card styles for reviewers to match other cards

--- frontend/src/components/PopularReviewers.js
+++ frontend/src/components/PopularReviewers.js
@@ -7,9 +7,11 @@ import { backendLink } from '../assets/config';
 const PopularReviewersStyles = styled.div`
   margin: 0 auto;
   display: flex;
+  justify-content: space-around;
   flex-flow: row wrap;
   width: 85%;
   height: auto;
+  min-height: 450px;
   @media (max-width: 900px) {
     background-color: white;
     width: 100%;
@@ -25,8 +27,20 @@ const PopularReviewersStyles = styled.div`
     color: black;
   }
   .users {
-    width: 200px;
-    margin-right: 25px;
+    box-sizing: border-box;
+    border-radius: 10px;
+    filter: drop-shadow(3px 3px 3px black);
+    padding: 10px;
+    background: #f1f1f1;
+    height: auto;
+    width: 20%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 50px;
+    line-height: 0.2;
+    text-decoration: none;
+    color: black;
     .users__name {
       font-size: 1.2rem;
     }
@@ -34,7 +48,9 @@ const PopularReviewersStyles = styled.div`
 `;
 
 export const CardStyle = styled.div`
-  margin-left: 15px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   img {
     max-width: 200px;
     height: 200px;
