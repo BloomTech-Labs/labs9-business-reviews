ff588d3ed41fdf2eb79b646fdd37cba042590338
bummings <eabummings@gmail.com>
2019-01-30 11:44:54 -0500

    adds relative paths for ENV file

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: '../../.env' });
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const authMiddleware = require('./authMiddlewares');
