24e063de8002a1d053343d1b457f66c73c15df80
bummings <eabummings@gmail.com>
2019-01-30 18:01:55 -0500

    includes isOpen variable to businessResult component

--- frontend/src/components/SearchResult.js
+++ frontend/src/components/SearchResult.js
@@ -14,7 +14,7 @@ const StyledBusiness = styled.div`
   display: flex;
   justify-content: center;
   background-image: url(${image});
-  line-height: 1.75;
+  line-height: 1.2;
   margin-bottom: 50px;
   .card {
     border: 1px solid grey;
@@ -28,11 +28,10 @@ const StyledBusiness = styled.div`
 
     .business__name {
       font-size: 4rem;
-      margin-top: -1.6rem;
     }
     .business__rating {
       font-size: 4rem;
-      margin-top: -4rem;
+      margin-top: -3.1rem;
     }
     .business__details {
       .business__details--address {
@@ -45,6 +44,13 @@ const StyledBusiness = styled.div`
         align-items: center;
         margin-bottom: 1rem;
       }
+
+      .business__details--hours {
+        display: flex;
+        align-items: center;
+        margin-bottom: 1rem;
+      }
+
       .business__details--website {
         display: flex;
         align-items: center;
@@ -133,6 +139,7 @@ class SearchResult extends React.Component {
       .then(res => this.setState({ reviews: res.data }))
       .catch(err => console.log(err));
   }
+
   addBusiness = () => {
     let imageCC = '';
     if (this.state.business.photos) {
@@ -170,15 +177,19 @@ class SearchResult extends React.Component {
       imageURL = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${imageCC}&key=${API_KEY}`;
     }
 
-    let isOpen = false;
+    let openBoolean = false;
+    let isOpen;
     if (!this.state.business) return <p>Loading business...</p>;
     else {
       if (
         this.state.business &&
         this.state.business.opening_hours !== undefined
       ) {
-        isOpen = this.state.business.opening_hours.open_now;
-        console.log(isOpen);
+        openBoolean = this.state.business.opening_hours.open_now;
+        console.log('vari: ', openBoolean);
+        isOpen = 'Open';
+      } else {
+        isOpen = 'Closed';
       }
       console.log(this.state.business);
       return (
@@ -227,7 +238,7 @@ class SearchResult extends React.Component {
                   >
                     <path d='M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm5.848 12.459c.202.038.202.333.001.372-1.907.361-6.045 1.111-6.547 1.111-.719 0-1.301-.582-1.301-1.301 0-.512.77-5.447 1.125-7.445.034-.192.312-.181.343.014l.985 6.238 5.394 1.011z' />
                   </svg>
-                  ${isOpen}
+                  <span>{isOpen}</span>
                 </div>
 
                 <div className='business__details--website'>
