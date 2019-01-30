770839907756294d80083ccbc0df8ec1b0312b86
bummings <eabummings@gmail.com>
2019-01-30 18:17:29 -0500

    re-adds cors-anywhere to searchResult

--- frontend/src/components/SearchResult.js
+++ frontend/src/components/SearchResult.js
@@ -130,7 +130,7 @@ class SearchResult extends React.Component {
   componentDidMount() {
     const { id } = this.props.match.params;
     Axios.get(
-      `https://maps.googleapis.com/maps/api/place/details/json?key=${API_KEY}&placeid=${id}`
+      `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/details/json?key=${API_KEY}&placeid=${id}`
     )
       .then(res => this.setState({ business: res.data.result }))
       .catch(err => console.log(err));
