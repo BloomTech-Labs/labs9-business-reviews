52bd65c02ef2cac2169d8ec257d3ea3bc2997b2b
Brad Mortensen <b.mort86@gmail.com>
2019-01-30 15:37:48 -0700

    reverts to production

--- app/index.js
+++ app/index.js
@@ -16,13 +16,13 @@ const port = process.env.PORT || 9000;
 // uncomment this when on development on localhost:3000 //
 // server.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 
-process.env.NODE_ENV === "production"
-  ? server.use(
-      cors({ credentials: true, origin: 'https://bonafind.netlify.com' })
-    )
-  : server.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+// process.env.NODE_ENV === "production"
+//   ? server.use(
+//       cors({ credentials: true, origin: 'https://bonafind.netlify.com' })
+//     )
+//   : server.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 // use this cors on production //
-// server.use(cors({ credentials: true, origin: 'https://bonafind.netlify.com' }));
+server.use(cors({ credentials: true, origin: 'https://bonafind.netlify.com' }));
 
 server.use(cookieParser());
 authMiddleware(server);
