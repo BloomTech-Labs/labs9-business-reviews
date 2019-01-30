b3b54fb17ef0bb676b7a54769bf1b323d5847ead
udykas <udykas@gmail.com>
2019-01-30 15:46:06 -0500

    Stripe Monthly router now adds customer before charging customer

--- app/stripe/stripeRouter.js
+++ app/stripe/stripeRouter.js
@@ -19,12 +19,20 @@ router.post('/yearly', authConfig.isLoggedIn, async (req, res) => {
 
 router.post('/monthly', authConfig.isLoggedIn, async (req, res) => {
   const [user] = req.user;
-  const { token, amount } = req.query;
-  const response = await stripe.charges.create({
-    source: token,
-    currency: 'USD',
-    amount
-  });
+  const { token } = req.query;
+  const email = token.email;
+  const respone = await stripe.customers.create({
+    email: email,
+    source: token
+  })
+  .then(customer => {
+    stripe.charges.create({
+      amount: 999,
+      description: "Monthly Subscription Charge",
+      currency: 'usd',
+      customer: customer.id
+    })
+  })
   user.subscription = Date.now() + 1000 * 60 * 60 * 24 * 30;
   await userModel.updateUser(user.id, user);
   res.json({ response });
