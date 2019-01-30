729c7fe50c785f1edb701823b48781173113d055
bummings <eabummings@gmail.com>
2019-01-29 23:50:01 -0500

    removes bonafind PNG include on landing nav component

--- frontend/src/components/LandingNav.js
+++ frontend/src/components/LandingNav.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import logo from '../assets/logo.png';
+// import logo from '../assets/logo.png';
 import SearchBar from './SearchBar';
 
 const NavBar = styled.div`
