c63feb5322096b0ea10a758bed4de7ba5580adda
Carlo Clamucha <carlo.clamucha@gmail.com>
2019-01-30 11:48:13 -0600

    adding developement configs

--- app/stripe/stripeRouter.js
+++ app/stripe/stripeRouter.js
@@ -1,41 +1,40 @@
 const express = require('express');
 const router = express.Router();
-const stripe = require("stripe")(process.env.STRIPE_SECRET);
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
+const authConfig = require('../user/authConfig');
+const userModel = require('../db/userModel/userModel');
 
-router.post('/yearly', async(req, res) => {
-  const {token} = req.query;
+router.post('/yearly', authConfig.isLoggedIn, async (req, res) => {
+  const [user] = req.user;
+  const { token } = req.query;
   const email = token.email;
   const response = await stripe.customers.create({
     email: email,
-    source: token
-  })
-  .then(customer => {
-    stripe.charges.create({
-      amount: 999,
-      description: "Yearly Subscription Charge",
-      currency: "usd",
-      customer: customer.id
-    })
-  })
-  res.json({response});
-})
+    source: token,
+    currency: 'USD'
+  });
+  user.subscription = new Date.now() + 1000 * 60 * 60 * 24 * 30;
+  userModel.updateUser(user.id, user);
+  res.json({ response });
+});
 
-router.post('/monthly', async(req, res) => {
-  const {token} = req.query;
+router.post('/monthly', authConfig.isLoggedIn, async (req, res) => {
+  const { token } = req.query;
   const email = token.email;
-  const response = await stripe.customers.create({
-    email: email,
-    source: token
-  })
-  .then(customer => {
-    stripe.charges.create({
-      amount: 99,
-      description: "Monthly Subscription Charge",
-      currency: "usd",
-      customer: customer.id
+  const response = await stripe.customers
+    .create({
+      email: email,
+      source: token
     })
-  })
-  res.json({response});
-})
+    .then(customer => {
+      stripe.charges.create({
+        amount: 99,
+        description: 'Monthly Subscription Charge',
+        currency: 'usd',
+        customer: customer.id
+      });
+    });
+  res.json({ response });
+});
 
 module.exports = router;
