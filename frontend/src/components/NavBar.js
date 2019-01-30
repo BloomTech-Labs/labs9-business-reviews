481876b5b212f0d397de2533fe80e88baa01eca7
Brad Mortensen <b.mort86@gmail.com>
2019-01-30 11:19:36 -0700

    adds ternary for login logout butotns

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,59 +1,67 @@
 import React from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { backendLink } from '../assets/config';
 import logo from '../assets/logo.png';
 
 const NavBarStyles = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  height: 130px;
-  background: #142e41;
-  .logo {
-    height: 100px;
-    width: 100px;
-    margin-left: 50px;
-  }
-  .nav-links {
-    width: 50%;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	width: 100%;
+	height: 130px;
+	background: #142e41;
+	.logo {
+		height: 100px;
+		width: 100px;
+		margin-left: 50px;
+	}
+	.nav-links {
+		width: 50%;
 		height: auto;
-  }
-  a {
-    color: white;
-    text-decoration: none;
-    padding-left: 50px;
-    margin-right: 100px;
-  }
-  img {
-    border-radius: 100px;
-  }
+	}
+	a,p {
+		color: white;
+		text-decoration: none;
+		padding-left: 50px;
+		margin-right: 100px;
+    cursor:pointer;
+	}
+	img {
+		border-radius: 100px;
+	}
 `;
 
-function NavBar() {
-  return (
-    <NavBarStyles>
-      <div className="logo_container">
-        <a href="/">
-          <img src={logo} alt="logo" className="logo" />
-        </a>
-      </div>
-      <div className="nav_links">
-        <a href="/addreview">add a review</a>
-        <a href="/categories">categories</a>
-        <a href="/toprated">top rated</a>
-      </div>
-      <div className="menu__user">
-        <Link to="/login" className="menu__user--text">
-          sign in
-        </Link>
-        <a href="/register" className="menu__user--text">
-          sign up
-        </a>
-      </div>
-    </NavBarStyles>
-  )
-  
+class NavBar extends React.Component {
+	state = { user: {} };
+	async componentDidMount() {
+		const res = await axios.get(`${backendLink}/api/user/me`, {
+			withCredentials: 'include'
+		});
+		if (!res.data.user) return null;
+		const [ user ] = res.data.user;
+		this.setState({ user });
+	}
+	render() {
+		return (
+			<NavBarStyles>
+				<div className="logo_container">
+					<a href="/">
+						<img src={logo} alt="logo" className="logo" />
+					</a>
+				</div>
+				<div className="nav_links">
+					<a href="/addreview">add a review</a>
+					<a href="/categories">categories</a>
+					<a href="/toprated">top rated</a>
+				</div>
+				<div className="menu__user">
+          {!this.state.user?<><Link to="/login" className="menu__user--text">sign in</Link><Link to="/register"className="menu__user--text">sign up</Link></>:<p>Sign Out</p>}					
+				</div>
+			</NavBarStyles>
+		);
+	}
 }
 
 export default NavBar;
