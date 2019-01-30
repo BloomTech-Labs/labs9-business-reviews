be9f1a6358996c2b11ced46a1a5511b7e6bc2689
bummings <eabummings@gmail.com>
2019-01-29 19:05:07 -0500

    reset

--- frontend/src/assets/config.js
+++ frontend/src/assets/config.js
@@ -1,6 +1,5 @@
 //Uncomment on Production
-export const backendLink = `https://bonafind.herokuapp.com`;
-//Uncomment on Development
-//export const backendLink = `http://localhost:9000`;
-// export const backendLink = `http://localhost:9000`;
+// export const backendLink = `https://bonafind.herokuapp.com`;
 
+//Uncomment on Development
+export const backendLink = `http://localhost:9000`;
