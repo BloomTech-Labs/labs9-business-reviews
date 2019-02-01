16ee32213279e1bd60a7bd05fd4db6e53345b3d6
bummings <eabummings@gmail.com>
2019-02-01 01:26:55 -0500

    adds github SVG to footer credit

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import github from '../assets/github.svg';
 
 const FooterStyles = styled.div`
   width: 100%;
@@ -19,15 +20,29 @@ const FooterStyles = styled.div`
     text-decoration: none;
     padding: 10px;
   }
+
+  .meetTheTeam {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .github {
+    filter: invert(1) sepia(0) saturate(0) hue-rotate(175deg);
+    margin-right: 0.6rem;
+  }
 `;
 
 function Footer() {
   return (
     <FooterStyles>
       <span>© 2019 Bonafind</span>
-      <a href='https://github.com/Lambda-School-Labs/labs9-business-reviews'>
-        Meet the Team
-      </a>
+      <div className='meetTheTeam'>
+        <img className='github' src={github} alt='' />
+        <a href='https://github.com/Lambda-School-Labs/labs9-business-reviews'>
+          Meet the Team
+        </a>
+      </div>
     </FooterStyles>
   );
 }
