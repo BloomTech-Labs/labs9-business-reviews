4f7defc2d43c18a16877996a6f459285b2582907
Brad Mortensen <b.mort86@gmail.com>
2019-02-04 13:53:33 -0700

    nav changes

--- frontend/src/components/LandingNav.js
+++ frontend/src/components/LandingNav.js
@@ -22,15 +22,42 @@ let backgrounds = [ bg1, bg2, bg3, bg4, bg5, bg6, bg7, bg8, bg9, bg10, bg11 ];
 
 const NavBar = styled.div`
 	width: 100%;
-	height: 600px;
+	height: auto;
 	background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
 		url(${backgrounds[Math.floor(Math.random() * backgrounds.length)]});
-
 	background-repeat: no-repeat;
 	background-position: center;
 	background-size: cover;
 	height: 75vh;
+	.logo {
+		animation: pulse 0.7s;
+		animation-iteration-count: 2;
+		animation-delay: 1s;
 
+		@media (max-height: 800px) {
+			width: 300px;
+			height: 325px;
+		}
+		@media (max-height: 690px) {
+			width: 250px;
+			height: 270px;
+		}
+
+		@media (max-height: 560px) {
+			display: none;
+		}
+	}
+
+	.logo__text {
+		display: none;
+		@media (max-height: 560px) {
+			display: inline-block;
+			font-family: Patua One;
+			font-size: 3.5rem;
+			color: white;
+			letter-spacing: 1rem;
+		}
+	}
 	.blur-container {
 		width: 100%;
 		height: 75px;
@@ -137,63 +164,60 @@ const NavBar = styled.div`
 `;
 
 class LandingNav extends React.Component {
-
-  state = { user: {}, loggedIn: false };
-  handleLogout = async () => {
-    const res = await axios.get(`${backendLink}/api/user/logout`, {
-      withCredentials: 'include'
-    });
-    res.status === 200
-      ? this.setState({ loggedIn: false })
-      : console.log('Error Logging Out');
-  };
-  async componentDidMount() {
-    const res = await axios.get(`${backendLink}/api/user/me`, {
-      withCredentials: 'include'
-    });
-    if (!res.data.user) return this.setState({ loggedIn: false });
-    const [user] = res.data.user;
-    this.setState({ user, loggedIn: true });
-  }
-  render() {
-    return (
-      <NavBar>
-        <div className='blur-container'>
-          <div className='top-nav-items'>
-            <div className='menu__menuItems'>
-              <a href='#Businesses'>businesses</a>
-              <a href='#Reviewers'>reviewers</a>
-              <a href='/subscription'>go pro ✔</a>
-            </div>
-            <div className='menu__user'>
-              {!this.state.loggedIn ? (
-                <React.Fragment>
-                  <Link to='/login' className='menu__user--text'>
-                    sign in
-                  </Link>
-                  <Link to='/register' className='menu__user--text'>
-                    sign up
-                  </Link>
-                </React.Fragment>
-              ) : (
-                <React.Fragment>
-                  <p onClick={this.handleLogout}>sign out</p>
-                  <Link to='/settings' className='menu__user--text'>
-                    settings
-                  </Link>
-                </React.Fragment>
-              )}
-            </div>
-          </div>
-        </div>
-        <div className='inner-nav-container'>
-          <img className='logo' src={logo} alt='logo' />
-          <p className='logo__text'>Bonafind</p>
-          <SearchBar />
-        </div>
-      </NavBar>
-    );
-  }
+	state = { user: {}, loggedIn: false };
+	handleLogout = async () => {
+		const res = await axios.get(`${backendLink}/api/user/logout`, {
+			withCredentials: 'include'
+		});
+		res.status === 200 ? this.setState({ loggedIn: false }) : console.log('Error Logging Out');
+	};
+	async componentDidMount() {
+		const res = await axios.get(`${backendLink}/api/user/me`, {
+			withCredentials: 'include'
+		});
+		if (!res.data.user) return this.setState({ loggedIn: false });
+		const [ user ] = res.data.user;
+		this.setState({ user, loggedIn: true });
+	}
+	render() {
+		return (
+			<NavBar>
+				<div className="blur-container">
+					<div className="top-nav-items">
+						<div className="menu__menuItems">
+							<a href="#Businesses">businesses</a>
+							<a href="#Reviewers">reviewers</a>
+							<a href="/subscription">go pro ✔</a>
+						</div>
+						<div className="menu__user">
+							{!this.state.loggedIn ? (
+								<React.Fragment>
+									<Link to="/login" className="menu__user--text">
+										sign in
+									</Link>
+									<Link to="/register" className="menu__user--text">
+										sign up
+									</Link>
+								</React.Fragment>
+							) : (
+								<React.Fragment>
+									<p onClick={this.handleLogout}>sign out</p>
+									<Link to="/settings" className="menu__user--text">
+										settings
+									</Link>
+								</React.Fragment>
+							)}
+						</div>
+					</div>
+				</div>
+				<div className="inner-nav-container">
+					<img className="logo" src={logo} alt="logo" />
+					<p className="logo__text">Bonafind</p>
+					<SearchBar />
+				</div>
+			</NavBar>
+		);
+	}
 }
 
 export default LandingNav;
