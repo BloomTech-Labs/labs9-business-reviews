673d84a4bba5a7ff91dc7461a49c2c98f1278821
udykas <udykas@gmail.com>
2019-02-04 19:15:51 -0500

    Added Modal stylings

--- frontend/src/components/SingleBusiness.js
+++ frontend/src/components/SingleBusiness.js
@@ -1,5 +1,26 @@
 import React from 'react'
-import { BackdropStyle } from './Modal'
+import styled from 'styled-components'
+
+const BackdropStyle = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgba(4, 0, 0, 0.7);
+  .modal{
+    width: 350px;
+    height: 500px;
+    border: 1px solid grey;
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-around;
+    background-color: white;
+  }
+`;
 
 
 function SingleBusiness({ toggleModal }) {
