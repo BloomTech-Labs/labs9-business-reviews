7a9e858ae622fbfbe0506da40cfa3920a73d6f4f
udykas <udykas@gmail.com>
2019-02-05 13:53:52 -0500

    Updated billing form text, updated snapshot and tests to check for correct div, p, and h1 elements

--- frontend/src/__tests__/BillingForm.test.js
+++ frontend/src/__tests__/BillingForm.test.js
@@ -11,4 +11,17 @@ describe('<BillingFormComponent />', () => {
     const wrapper = shallow(<BillingFormComponent />);
     expect(toJSON(wrapper)).toMatchSnapshot();
   });
+  it('should contain 1 div', () => {
+    let app = shallow(<BillingFormComponent />);
+    expect(app.find("div").length).toBe(1);
+  });
+  it('should contain 1 p tag', () => {
+    let app = shallow(<BillingFormComponent />);
+    expect(app.find("p").length).toBe(1);
+  });
+  it('should contain 1 h1 element', () => {
+    let app = shallow(<BillingFormComponent />);
+    expect(app.find("h1").length).toBe(1);
+    expect(app.find('h1').text()).toMatch("Bonafind Subscription");
+  });
 });
