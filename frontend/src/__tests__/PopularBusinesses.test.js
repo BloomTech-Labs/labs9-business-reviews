eedd93125de4dd68f0635912a3e63b3ece2ef6a1
udykas <udykas@gmail.com>
2019-02-05 17:10:50 -0500

    Checks for div with businesses id

--- frontend/src/__tests__/PopularBusinesses.test.js
+++ frontend/src/__tests__/PopularBusinesses.test.js
@@ -11,4 +11,13 @@ describe('PopularBusinessesComponent />', () => {
     const wrapper = shallow(<PopularBusinessesComponent />);
     expect(toJSON(wrapper)).toMatchSnapshot();
   });
+  it('should contain h1.business__header with text "Popular Businesses', () => {
+    let app = shallow(<PopularBusinessesComponent/>);
+    expect(app.find(".business__header").length).toBe(1);
+    expect(app.find(".business__header").text()).toBe("Popular Businesses");
+  });
+  it('should contain div with id #Businesses', () => {
+    let app = shallow(<PopularBusinessesComponent/>);
+    expect(app.find("#Businesses").length).toBe(1);
+  });
 });
