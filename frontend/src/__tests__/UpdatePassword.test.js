f39d70f163447b06b4cae9d7a71a5b8164ad655d
udykas <udykas@gmail.com>
2019-02-05 17:53:39 -0500

    Checks for Navbar, div elements and form

--- frontend/src/__tests__/UpdatePassword.test.js
+++ frontend/src/__tests__/UpdatePassword.test.js
@@ -11,4 +11,17 @@ describe('UpdatePasswordComponent />', () => {
     const wrapper = shallow(<UpdatePasswordComponent />);
     expect(toJSON(wrapper)).toMatchSnapshot();
   });
+  it('should contain NavBar', () => {
+    let app = shallow(<UpdatePasswordComponent />);
+    expect(app.find("NavBar").length).toBe(1);
+  });
+  it('should contain two div elements', () => {
+    let app = shallow(<UpdatePasswordComponent />);
+    expect(app.find("div").length).toBe(2);
+  });
+  it('should contain one form', () => {
+    let app = shallow(<UpdatePasswordComponent />);
+    expect(app.find("form").length).toBe(1);
+  });
 });
+
