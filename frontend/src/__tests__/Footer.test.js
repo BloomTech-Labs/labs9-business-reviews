277c01db3862fbb3a46b8cc2bf4e37bc39afaf12
udykas <udykas@gmail.com>
2019-02-05 15:45:39 -0500

    Checks for correct number of div, span, img and anchor elements and that the text for the anchor is correct

--- frontend/src/__tests__/Footer.test.js
+++ frontend/src/__tests__/Footer.test.js
@@ -11,4 +11,21 @@ describe('<FooterComponent />', () => {
     const wrapper = shallow(<FooterComponent />);
     expect(toJSON(wrapper)).toMatchSnapshot();
   });
+  it('should contain 1 span', () => {
+    let app = shallow(<FooterComponent />);
+    expect(app.find("span").length).toBe(1);
+  });
+  it('should contain 1 div', () => {
+    let app = shallow(<FooterComponent />);
+    expect(app.find("div").length).toBe(1);
+  });
+  it('should contain 1 img', () => {
+    let app = shallow(<FooterComponent />);
+    expect(app.find("img").length).toBe(1);
+  });
+  it('should contain 1 "a" tag with text "Meet the Team"', () => {
+    let app = shallow(<FooterComponent />);
+    expect(app.find("a").length).toBe(1);
+    expect(app.find("a").text()).toMatch("Meet the Team");
+  });
 });
