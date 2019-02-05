1173e0ae5aba7811aa50fc674b4acba6ce857fd9
Carlo Clamucha <carlo.clamucha@gmail.com>
2019-02-05 17:21:45 -0600

    added a ternary link

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -9,6 +9,7 @@ const SearchBarStyles = styled.div`
   align-items: center;
   padding-top: 2rem;
   padding-bottom: 2rem;
+  transition: 1s all;
 
   @media (max-height: 690px) {
     padding-top: 0;
@@ -91,30 +92,42 @@ class SearchBar extends React.Component {
           rating: 0,
           website: ''
         }
-      ]
+      ],
+      inputVal: ''
     };
   }
 
   componentDidMount() {
     const input = document.querySelector('#dropdown'); /// yooooooooo
+
     const dropdown = new window.google.maps.places.Autocomplete(input);
     dropdown.addListener('place_changed', () => {
       const place = dropdown.getPlace();
       this.setState({ business: place });
     });
   }
+  handleChange = e => {
+    this.setState({ inputVal: e.target.value });
+  };
 
   render() {
     return (
       <SearchBarStyles>
         <form>
           <input
-            className='search__input'
-            id='dropdown'
-            placeholder='What are you looking for?'
+            onChange={this.handleChange}
+            className="search__input"
+            id="dropdown"
+            placeholder="What are you looking for?"
           />
-          <Link to={`/business/${this.state.business.place_id}`}>
-            <button className='button'>search</button>
+          <Link
+            to={
+              this.state.inputVal
+                ? `/business/${this.state.business.place_id}`
+                : '/'
+            }
+          >
+            <button className="button">search</button>
           </Link>
         </form>
       </SearchBarStyles>
