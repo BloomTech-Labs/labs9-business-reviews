0a958620d4eede401841d5bbc2c1855ec8c1bd13
udykas <udykas@gmail.com>
2019-02-05 11:59:18 -0500

    Added test to ensure components are in LandingPage

--- frontend/src/__tests__/LandingPage.test.js
+++ frontend/src/__tests__/LandingPage.test.js
@@ -11,4 +11,9 @@ describe('<LandingPageComponent />', () => {
     const wrapper = shallow(<LandingPageComponent />);
     expect(toJSON(wrapper)).toMatchSnapshot();
   });
+  it('should contain LandingNav and Maincontent', () => {
+    let land = shallow(<LandingPageComponent />);
+    expect(land.find("LandingNav").length).toBe(1);
+    expect(land.find("MainContent").length).toBe(1);
+  })
 });
