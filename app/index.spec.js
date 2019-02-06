fda2df1b2a8ec34f51a38cb8c937fc0dd29884c2
udykas <udykas@gmail.com>
2019-02-06 12:13:31 -0500

    Added boilerplate for server test

--- app/index.spec.js
+++ app/index.spec.js
@@ -1,3 +1,9 @@
 const request = require('supertest');
 const server = require('./index');
 
+describe('index.js', () => {
+	it('should return status code 200 and JSON object from index route', async() => {
+		const statusCode = 200;
+		const expectedBody = { api: 'API root.' }
+	})
+})
