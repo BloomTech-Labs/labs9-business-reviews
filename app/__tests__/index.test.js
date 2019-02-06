318330414e78c511f677b3b0ab026862ccfd68a5
udykas <udykas@gmail.com>
2019-02-06 13:19:28 -0500

    Switched to json

--- app/__tests__/index.test.js
+++ app/__tests__/index.test.js
@@ -4,13 +4,12 @@ const server = require('../index');
 describe('index.js', () => {
 	it('should return status code 200 and JSON object from index route', async() => {
 		const statusCode = 200;
-		const expectedBody = 'API root.';
+		const expectedBody = { api: 'API root.' };
 
 		const response = await request(server).get('/');
 
 		expect(response.status).toEqual(statusCode);
-		expect(response.type).toEqual('text/html');
+		expect(response.type).toEqual('application/json');
 		expect(response.body).toEqual(expectedBody);
-
 	})
 })
