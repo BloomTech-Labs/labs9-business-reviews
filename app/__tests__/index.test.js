7f57a2f4bd3fcb9ed7713383a834023d46790180
udykas <udykas@gmail.com>
2019-02-06 12:51:52 -0500

    Business tests all pass now

--- app/__tests__/index.test.js
+++ app/__tests__/index.test.js
@@ -4,12 +4,13 @@ const server = require('../index');
 describe('index.js', () => {
 	it('should return status code 200 and JSON object from index route', async() => {
 		const statusCode = 200;
-		const expectedBody = { api: 'API root.' };
+		const expectedBody = 'API root.';
 
 		const response = await request(server).get('/');
 
 		expect(response.status).toEqual(statusCode);
-		expect(response.type).toEqual('application/json');
+		expect(response.type).toEqual('text/html');
 		expect(response.body).toEqual(expectedBody);
+
 	})
 })
