b52cfe0cf9463dc2a183825dbea200a0851b8da2
udykas <udykas@gmail.com>
2019-02-06 12:19:05 -0500

    Added tests to check status code, response type and correct body, also moved to test folder

--- app/index.spec.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest');
-const server = require('./index');
-
-describe('index.js', () => {
-	it('should return status code 200 and JSON object from index route', async() => {
-		const statusCode = 200;
-		const expectedBody = { api: 'API root.' }
-	})
-})
