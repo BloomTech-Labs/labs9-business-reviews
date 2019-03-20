5adfecd107745b7fb1e8e801cd4284674db754d8
unknown <jesse.reichel@live.com>
2019-03-20 14:33:00 -0700

    reverted back to production

--- frontend/src/assets/config.js
+++ frontend/src/assets/config.js
@@ -1,10 +1,10 @@
 //Uncomment on Production
-// export const backendLink = `https://bonafind.herokuapp.com`;
+export const backendLink = `https://bonafind.herokuapp.com`;
 
 // export const backendLink =
 //   process.env.NODE_ENV === 'production'
 //     ? `https://bonafind.herokuapp.com`
 //     : `http://localhost:9000`;
 
-//Uncomment on Development
-export const backendLink = `http://localhost:9000`;
+// //Uncomment on Development
+// export const backendLink = `http://localhost:9000`;
