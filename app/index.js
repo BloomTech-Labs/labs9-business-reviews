26157df19c55cc84c521d33e571a14f4e8c91e93
unknown <jesse.reichel@live.com>
2019-03-20 20:02:24 -0700

    reverted to self deployed option to screenshot errors

--- app/index.js
+++ app/index.js
@@ -25,8 +25,8 @@ const port = process.env.PORT || 9000;
 server.use(
   cors({
     credentials: true,
-    origin: 'https://bonafind.netlify.com',
-    AccessControlAllowOrigin: 'https://bonafind.herokuapp.com'
+    origin: 'https://objective-curie-345595.netlify.com',
+    AccessControlAllowOrigin: 'https://arcane-headland-99879.herokuapp.com'
   })
 );
 
